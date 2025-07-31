792abd6a05d955d0ca80bfe54f205bf56411a278
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2025-07-31 14:10:57 +0300

    Update print_utils.py

--- print_utils.py
+++ print_utils.py
@@ -1,11 +1,11 @@
-def print_info(text):
-    print(f"\033[94m{text}\033[0m")  # Blue
+def print_info(text, end='\n'):
+    print(f"\033[94m{text}\033[0m", end=end)  # Blue
 
-def print_warn(text):
-    print(f"\033[93m{text}\033[0m")  # Yellow
+def print_warn(text, end='\n'):
+    print(f"\033[93m{text}\033[0m", end=end)  # Yellow
 
-def print_error(text):
-    print(f"\033[91m{text}\033[0m")  # Red
+def print_error(text, end='\n'):
+    print(f"\033[91m{text}\033[0m", end=end)  # Red
 
-def print_success(text):
-    print(f"\033[92m{text}\033[0m")  # Green
+def print_success(text, end='\n'):
+    print(f"\033[92m{text}\033[0m", end=end)  # Green
