ad252bfd7ed7ab84e9fab639b73178b9979c5b5e
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2025-07-31 14:10:28 +0300

    Add print_utils.py

---
+++ print_utils.py
@@ -0,0 +1,11 @@
+def print_info(text):
+    print(f"\033[94m{text}\033[0m")  # Blue
+
+def print_warn(text):
+    print(f"\033[93m{text}\033[0m")  # Yellow
+
+def print_error(text):
+    print(f"\033[91m{text}\033[0m")  # Red
+
+def print_success(text):
+    print(f"\033[92m{text}\033[0m")  # Green
